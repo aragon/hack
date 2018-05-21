023554d930841a659ffa3ec4e385dbb00da8866b
Luis Cuende <me@luisivan.net>
2018-05-21 05:57:56 +0200

    hack.aragon.org

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -28,9 +28,9 @@ const users = [
 const siteConfig = {
   title: 'Aragon Developer Portal',
   tagline: 'What you need to get started building with Aragon',
-  url: 'https://hack.aragon.one',
+  url: 'https://hack.aragon.org',
   baseUrl: '/',
-  cname: 'hack.aragon.one',
+  cname: 'hack.aragon.org',
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
