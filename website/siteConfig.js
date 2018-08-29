71bba3117b775259e5c8afc3040cd9f1ab92cb88
Luis Cuende <me@luisivan.net>
2018-08-29 14:50:40 +0200

    Added Piwik analytics

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -109,6 +109,20 @@ const siteConfig = {
     const insertStylesAt = html.lastIndexOf('</body>')
     return `
       <!DOCTYPE html>
+      <script type="text/javascript">
+        var _paq = _paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="https://arastats.eu/staats/";
+          _paq.push(['setTrackerUrl', u+'piwik.php']);
+          _paq.push(['setSiteId', '7']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+        })();
+      </script>
+      <noscript><img src="https://arastats.eu/staats/piwik.php?idsite=7&amp;rec=1" style="border:0" alt="" /></noscript>
       ${html.slice(0, insertStylesAt)}
       ${sheet.getStyleTags()}
       ${html.slice(insertStylesAt)}
