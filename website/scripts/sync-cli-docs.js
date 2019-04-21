cd98209c6693ad31091579bde990204c5ed83ff7
Daniel Constantin <hello@danielconstantin.net>
2019-04-21 13:01:12 +0300

    Add CLI -> IPFS docs (#136)
    Closes #126

--- website/scripts/sync-cli-docs.js
+++ website/scripts/sync-cli-docs.js
@@ -11,36 +11,43 @@ const pages = [
     title: 'Using the aragonCLI',
     sidebarLabel: 'Introduction',
     hideTitle: true,
-    contentURL: `${BASE_CONTENT_URL}/Intro.md`
+    contentURL: `${BASE_CONTENT_URL}/Intro.md`,
   },
   {
     fileLocation: '/docs/cli-main-commands.md',
     id: 'cli-main-commands',
     title: 'Main commands',
     sidebarLabel: 'Main commands',
-    contentURL: `${BASE_CONTENT_URL}/Main-commands.md`
+    contentURL: `${BASE_CONTENT_URL}/Main-commands.md`,
   },
   {
     fileLocation: '/docs/cli-apm-commands.md',
     id: 'cli-apm-commands',
     title: 'aragonPM commands',
-    sidebarLabel: 'aragonPM commands',
-    contentURL: `${BASE_CONTENT_URL}/Apm-commands.md`
+    sidebarLabel: 'APM commands',
+    contentURL: `${BASE_CONTENT_URL}/Apm-commands.md`,
   },
   {
     fileLocation: '/docs/cli-dao-commands.md',
     id: 'cli-dao-commands',
     title: 'DAO commands',
     sidebarLabel: 'DAO commands',
-    contentURL: `${BASE_CONTENT_URL}/Dao-commands.md`
+    contentURL: `${BASE_CONTENT_URL}/Dao-commands.md`,
+  },
+  {
+    fileLocation: '/docs/cli-ipfs-commands.md',
+    id: 'cli-ipfs-commands',
+    title: 'IPFS commands',
+    sidebarLabel: 'IPFS commands',
+    contentURL: `${BASE_CONTENT_URL}/Ipfs-commands.md`,
   },
   {
     fileLocation: '/docs/cli-global-confg.md',
     id: 'cli-global-confg',
     title: 'Global configuration',
     sidebarLabel: 'Global configuration',
-    contentURL: `${BASE_CONTENT_URL}/Global-confg.md`
-  }
+    contentURL: `${BASE_CONTENT_URL}/Global-confg.md`,
+  },
 ]
 
 const locationReferenceMap = {}
