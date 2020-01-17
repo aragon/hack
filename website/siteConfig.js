59f6bae2338046ab433d1550fa137a0e846e7bd1
Brett Sun <qisheng.brett.sun@gmail.com>
2020-01-17 19:40:56 +0800

    Fix Matomo (#212)

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -139,20 +139,21 @@ const siteConfig = {
     const insertStylesAt = html.lastIndexOf('</body>')
     return `
       <!DOCTYPE html>
+      <!-- Matomo -->
       <script type="text/javascript">
-        var _paq = _paq || [];
+        var _paq = window._paq || [];
         /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
         _paq.push(['trackPageView']);
         _paq.push(['enableLinkTracking']);
         (function() {
-          var u="https://arastats.eu/staats/";
-          _paq.push(['setTrackerUrl', u+'piwik.php']);
+          var u="//arastats.eu/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
           _paq.push(['setSiteId', '7']);
           var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-          g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+          g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
         })();
       </script>
-      <noscript><img src="https://arastats.eu/staats/piwik.php?idsite=7&amp;rec=1" style="border:0" alt="" /></noscript>
+      <!-- End Matomo Code -->
       ${html.slice(0, insertStylesAt)}
       ${sheet.getStyleTags()}
       ${html.slice(insertStylesAt)}
